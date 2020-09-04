2dfc95695d2b2a55cc6f228eaaee59c37ab5cbfb
ysouyno <ysouyno@163.com>
2020-09-04 16:46:09 +0800

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![crate_name = "rs_travis_demo"]
+#![crate_type = "lib"]
+
+//! This is a comment.
+
+/// This function always returns true. It's very useful!
+pub fn always_true() -> bool {
+    true
+}
+
+#[test]
+fn it_works() {
+    assert_eq!(always_true(), true);
+}
